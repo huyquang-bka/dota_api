748da8c6a4f6908f1363f53d838d7612528575d1
Quang <huyquangbka@gmail.com>
2021-09-30 14:50:31 +0700

    first

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+import requests
+import json
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+with open("jsonFile/hero_ids.json", "rb") as f:
+    hero_ids = json.load(f)
+
+with open("jsonFile/item_ids.json", "rb") as f:
+    item_ids = json.load(f)
+
+
+@app.route("/match_info/<match_id>")
+def match_info(match_id):
+    r = requests.get(f"https://api.opendota.com/api/matches/{match_id}/")
+    data = json.loads(r.content)
+    match_dict = {}
+    for player in data["players"]:
+        hero_info = {}
+        account_id = player["account_id"]
+        account_name = json.loads(requests.get(f"https://api.opendota.com/api/players/{account_id}/").content)["profile"]["personaname"]
+        hero_id = player["hero_id"]
+        hero_info["hero_name"] = hero_ids[str(hero_id)]
+        hero_info["level"] = player["level"]
+        hero_info["net_worth"] = player["net_worth"]
+        hero_info["kills"] = player["kills"]
+        hero_info["deaths"] = player["deaths"]
+        hero_info["assists"] = player["assists"]
+        hero_info["damage"] = player["hero_damage"]
+        hero_info["items"] = []
+        for i in range(6):
+            hero_info["items"].append(item_ids[str(player[f"item_{i}"])])
+        match_dict[account_name] = hero_info
+
+    return jsonify(match_dict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
