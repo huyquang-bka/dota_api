f924645163760a0cf27d6dd911ad1a0fb710af51
Quang <huyquangbka@gmail.com>
2021-09-30 15:11:58 +0700

    test

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import json
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
-with open("jsonFile/hero_ids.json", "rb") as f:
-    hero_ids = json.load(f)
-
-with open("jsonFile/item_ids.json", "rb") as f:
-    item_ids = json.load(f)
+# with open("jsonFile/hero_ids.json", "rb") as f:
+#     hero_ids = json.load(f)
+#
+# with open("jsonFile/item_ids.json", "rb") as f:
+#     item_ids = json.load(f)
 
 
 @app.route("/")
@@ -17,29 +17,29 @@ def first_page():
     return "This is for Dota Api"
 
 
-@app.route("/match_info/<match_id>")
-def match_info(match_id):
-    r = requests.get(f"https://api.opendota.com/api/matches/{match_id}/")
-    data = json.loads(r.content)
-    match_dict = {}
-    for player in data["players"]:
-        hero_info = {}
-        account_id = player["account_id"]
-        account_name = json.loads(requests.get(f"https://api.opendota.com/api/players/{account_id}/").content)["profile"]["personaname"]
-        hero_id = player["hero_id"]
-        hero_info["hero_name"] = hero_ids[str(hero_id)]
-        hero_info["level"] = player["level"]
-        hero_info["net_worth"] = player["net_worth"]
-        hero_info["kills"] = player["kills"]
-        hero_info["deaths"] = player["deaths"]
-        hero_info["assists"] = player["assists"]
-        hero_info["damage"] = player["hero_damage"]
-        hero_info["items"] = []
-        for i in range(6):
-            hero_info["items"].append(item_ids[str(player[f"item_{i}"])])
-        match_dict[account_name] = hero_info
-
-    return jsonify(match_dict)
+# @app.route("/match_info/<match_id>")
+# def match_info(match_id):
+#     r = requests.get(f"https://api.opendota.com/api/matches/{match_id}/")
+#     data = json.loads(r.content)
+#     match_dict = {}
+#     for player in data["players"]:
+#         hero_info = {}
+#         account_id = player["account_id"]
+#         account_name = json.loads(requests.get(f"https://api.opendota.com/api/players/{account_id}/").content)["profile"]["personaname"]
+#         hero_id = player["hero_id"]
+#         hero_info["hero_name"] = hero_ids[str(hero_id)]
+#         hero_info["level"] = player["level"]
+#         hero_info["net_worth"] = player["net_worth"]
+#         hero_info["kills"] = player["kills"]
+#         hero_info["deaths"] = player["deaths"]
+#         hero_info["assists"] = player["assists"]
+#         hero_info["damage"] = player["hero_damage"]
+#         hero_info["items"] = []
+#         for i in range(6):
+#             hero_info["items"].append(item_ids[str(player[f"item_{i}"])])
+#         match_dict[account_name] = hero_info
+#
+#     return jsonify(match_dict)
 
 
 if __name__ == "__main__":
