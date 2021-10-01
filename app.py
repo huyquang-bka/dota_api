960aa1cffae90bf4dc7ab0a0d8a66e1b98e9589f
Quang <huyquangbka@gmail.com>
2021-10-01 11:10:03 +0700

    fix hero info

--- app.py
+++ app.py
@@ -22,27 +22,27 @@ def match_info(match_id):
     r = requests.get(f"https://api.opendota.com/api/matches/{match_id}/")
     data = json.loads(r.content)
     match_dict = {}
+    hero_info = {}
     count = 0
     for player in data["players"]:
-        hero_info = {}
         account_id = player["account_id"]
         account_name = json.loads(requests.get(f"https://api.opendota.com/api/players/{account_id}/").content)["profile"]["personaname"]
-        hero_info["player_name"] = account_name
+        hero_info[f"player_name_{count}"] = account_name
         hero_id = player["hero_id"]
-        hero_info["hero_name"] = hero_ids[str(hero_id)]
-        hero_info["level"] = player["level"]
-        hero_info["net_worth"] = player["net_worth"]
-        hero_info["kills"] = player["kills"]
-        hero_info["deaths"] = player["deaths"]
-        hero_info["assists"] = player["assists"]
-        hero_info["damage"] = player["hero_damage"]
-        hero_info["items"] = []
+        hero_info[f"hero_name_{count}"] = hero_ids[str(hero_id)]
+        hero_info[f"level_{count}"] = player["level"]
+        hero_info[f"net_worth_{count}"] = player["net_worth"]
+        hero_info[f"kills_{count}"] = player["kills"]
+        hero_info[f"deaths_{count}"] = player["deaths"]
+        hero_info[f"assists_{count}"] = player["assists"]
+        hero_info[f"damage_{count}"] = player["hero_damage"]
+        hero_info[f"items_{count}"] = []
         for i in range(6):
-            hero_info["items"].append(item_ids[str(player[f"item_{i}"])])
-        match_dict[count] = hero_info
+            hero_info[f"items_{count}"].append(item_ids[str(player[f"item_{i}"])])
+        # match_dict[count] = hero_info
         count += 1
 
-    return jsonify(match_dict)
+    return jsonify(hero_info)
 
 
 if __name__ == "__main__":
